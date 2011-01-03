03e8cce402c272247ecf6b8b5c1676da2147ace9
cellarosi <cellarosi@localhost>
2011-01-03 22:42:19 +0100

    Added south app

--- settings.py
+++ settings.py
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'booking',
     'entity',
     'rosetta',
+    'south',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
